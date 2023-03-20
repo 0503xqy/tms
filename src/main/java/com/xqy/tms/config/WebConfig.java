80f9413b63377f8c1b42ae0d7f3e366e3799b625
0503xqy <18821911543@163.com>
2023-03-20 22:43:42 +0800

    开放拦截

--- src/main/java/com/xqy/tms/config/WebConfig.java
+++ src/main/java/com/xqy/tms/config/WebConfig.java
@@ -15,12 +15,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//    /**
-//     * 添加拦截器
-//     */
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        //拦截路径可自行配置多个 可用 ，分隔开
-//        registry.addInterceptor(new JwtInterceptor()).addPathPatterns("/**").excludePathPatterns("/magic/**");
-//    }
+    /**
+     * 添加拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截路径可自行配置多个 可用 ，分隔开
+        registry.addInterceptor(new JwtInterceptor()).addPathPatterns("/**").excludePathPatterns("/magic/**");
+    }
 }
